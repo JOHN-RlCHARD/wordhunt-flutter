ec4f611009ae91593d5510f21a4eb3d0f892e597
John Richard <joao.ensenat@gmail.com>
2023-05-03 10:10:28 -0300

    background animation

--- wordhunt_phaser/lib/components/background_component.dart
+++ wordhunt_phaser/lib/components/background_component.dart
@@ -1,14 +1,24 @@
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 import 'package:wordhunt_phaser/games/wordhunt_game.dart';
 import '../constants/globals.dart';
 
-class BackgroundComponent extends SpriteComponent
+class BackgroundComponent extends SpriteAnimationComponent
     with HasGameRef<WordHuntGame> {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
-    sprite = await gameRef.loadSprite(Globals.backgroundSprite);
     size = gameRef.size;
+
+    final data = SpriteAnimationData.sequenced(
+      textureSize: Vector2(640, 360),
+      amount: 36,
+      amountPerRow: 6,
+      stepTime: 0.15,
+    );
+
+    animation = SpriteAnimation.fromFrameData(
+        await gameRef.images.load(Globals.backgroundSprite), data);
   }
 }
